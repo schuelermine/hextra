c53c0845e9bad4abbdec5fbc6dd37447c11c2890
schuelermine <nitrogenfan@gmail.com>
2019-05-11 10:20:04 +0200

    Added Trees

--- Data/Tree.hs
+++ Data/Tree.hs
@@ -1,3 +1,7 @@
 module Data.Tree where
 -- Defines Tree types
 
+data Tree f a = Split a (f (Tree f a)) | End
+data Skeleton f = Joint (f (Skeleton f)) | Condyle
+data NonEmptyTree f a = Node a (f (NonEmptyTree f a)) | Leaf a
+data Flower f a = Branch (f (Flower f a)) | Blossom a
