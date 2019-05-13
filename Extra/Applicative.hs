1bc73294a387438810aa898cd41608db3d9bdb53
schuelermine <nitrogenfan@gmail.com>
2019-05-13 22:44:08 +0200

    Added to Applicative.hs

--- Extra/Applicative.hs
+++ Extra/Applicative.hs
@@ -1,7 +1,18 @@
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+
 module Extra.Applicative where
 
-zipA :: Applicative f => f a -> f b -> f (a, b)
-zipA a b = (,) <$> a <*> b
+(<:>) :: Applicative f => f a -> f b -> f (a, b)
+(<:>) a b = (,) <$> a <*> b
 
 unit :: Applicative f => f ()
 unit = pure ()
+
+(<.>) :: Applicative f => f (b -> c) -> f (a -> b) -> f (a -> c)
+(<.>) f g = (.) <$> f <*> g
+
+mkApp :: Functor f => (forall x y. f x -> f y -> f (x, y)) -> f (a -> b) -> f a -> f b
+mkApp (?) f x = fmap (uncurry ($)) $ f ? x
+
+mkPure :: Functor f => (f ()) -> a -> f a
+mkPure u a = fmap (const a) u
