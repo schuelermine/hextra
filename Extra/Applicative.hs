df0385a449bcb8ed1fd7e20663d6181ee5f0dac8
Lessing-Gymnasium Berlin Medienzentrum Laptop #10669 <lessinggast@gmail.com>
2019-05-14 11:13:23 +0100

    Added Monoidal class

--- Extra/Applicative.hs
+++ Extra/Applicative.hs
@@ -26,3 +26,13 @@ mkPure :: Functor f => (f ()) -> a -> f a
 mkPure u a = fmap (const a) u
 -- Creates a pure definition from a definition of unit
 -- mkPure unit = pure
+
+class Monoidal f where
+    nilA :: f ()
+    zipA :: f a -> f b -> f (a, b)
+
+mkZipA :: Functor f => (forall x y. f (x -> y) -> f x -> f y) -> f a -> f b -> f (a, b)
+mkZipA (?) x y = (,) <$> x ? y
+
+mkNilA :: Functor f => (forall x. x -> f x) -> f ()
+mkNilA p = p ()
