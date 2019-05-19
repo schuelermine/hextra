a42108f60e4480d57d12b8cb74c715cb45e1849d
schuelermine <nitrogenfan@gmail.com>
2019-05-19 22:23:36 +0200

    moved some definitions in Extra.Kind...

--- Extra/Kind/Hetero.hs
+++ Extra/Kind/Hetero.hs
@@ -2,6 +2,17 @@
 
 module Extra.Kind.Hetero where
 
-import Extra.Kind.Constraint
+data InstanceOf f where
+    Of :: forall f a. f a => a -> InstanceOf f
+    
+instance Show (InstanceOf Show) where
+    showsPrec i (Of a) = showsPrec i a
+    show (Of a) = show a
 
 type ClassContainer g f = g (InstanceOf f)
+
+data Potentially a where
+    Is :: forall a b. (b -> a) -> b -> Potentially a
+
+getDefinite :: Potentially a -> a
+getDefinite (Is f a) = f a
--- Extra/Kind/Instance.hs
+++ Extra/Kind/Instance.hs
@@ -1,19 +1,12 @@
 {-# LANGUAGE TypeInType, DataKinds, ConstraintKinds, GADTs, TypeFamilies, MultiParamTypeClasses, ExplicitForAll, FlexibleInstances, UndecidableInstances, QuantifiedConstraints, RankNTypes, TypeOperators, FunctionalDependencies, ScopedTypeVariables, KindSignatures, NoStarIsType, PolyKinds, StandaloneDeriving #-}
 
-module Extra.Kind.Constraint where
+module Extra.Kind.Instance where
 
 import Data.Kind
 
 class Always a
 instance Always a
 
-data InstanceOf f where
-    Of :: f a => a -> InstanceOf f
-
 type family FoldConstraint (l :: [k -> Constraint]) (a :: k) :: Constraint
 type instance FoldConstraint '[] _ = ()
 type instance FoldConstraint (x ': xs) a = (x a, FoldConstraint xs a)
-
-instance Show (InstanceOf Show) where
-    showsPrec i (Of a) = showsPrec i a
-    show (Of a) = show a
