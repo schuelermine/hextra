e4e8d5e811f77bad2d9e9b32475e87cd98bea9ab
schuelermine <nitrogenfan@gmail.com>
2019-06-04 11:19:25 +0200

    added readpromptfor and co

--- Extra.hs
+++ Extra.hs
@@ -16,3 +16,9 @@ iff a b c = if a then b else c
 (<=>) = compare
 (?+) = max
 (?-) = min
+
+readpromptfor :: Read a => (a -> IO b) -> IO b
+readpromptfor f = (getLine >>= f . read)
+
+promptfor :: (String -> IO b) -> IO b
+promptfor = (getLine >>=)
