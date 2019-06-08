508f06f8ecaabed6b0e455523958cde2851fab01
schuelermine <nitrogenfan@gmail.com>
2019-06-08 22:56:09 +0200

    Renamed Sqc to Stream

--- Data/Sqc.hs
+++ Data/Sqc.hs
@@ -1,37 +1,37 @@
 {-# LANGUAGE ExplicitForAll #-}
 
-module Data.Sqc where
--- Defines the Sqc (pronounce: Sequence) type and related functions
+module Data.Stream where
+-- Defines the Stream (pronounce: Sequence) type and related functions
 
 import Extra.Tuple as Tup
 
-data Sqc a = a :- Sqc a
+data Stream a = a :- Stream a
 -- Type for infinite lists
 -- Pronounce: Sequence
 
-sqcHead :: forall a. Sqc a -> a
-sqcHead (x :- _) = x
+streamHead :: forall a. Stream a -> a
+streamHead (x :- _) = x
 
-sqcTail :: forall a. Sqc a -> Sqc a
-sqcTail (_ :- xs) = xs
--- Like tail, but for Sqc
--- Safer, since a Sqc can't be empty
+streamTail :: forall a. Stream a -> Stream a
+streamTail (_ :- xs) = xs
+-- Like tail, but for Stream
+-- Safer, since a Stream can't be empty
 
-sqcFromList :: forall a. [a] -> a -> Sqc a
-sqcFromList [] a = sqcUnfold dupe a
-sqcFromList (x:xs) a = x :- sqcFromList xs a
--- Makes a Sqc from a finite list, repeats second argument when list is exhausted
+streamFromList :: forall a. [a] -> a -> Stream a
+streamFromList [] a = streamUnfold dupe a
+streamFromList (x:xs) a = x :- streamFromList xs a
+-- Makes a Stream from a finite list, repeats second argument when list is exhausted
 
-sqcUnfold :: forall a b. (a -> (a, b)) -> a -> Sqc b
-sqcUnfold f x =
+streamUnfold :: forall a b. (a -> (a, b)) -> a -> Stream b
+streamUnfold f x =
     let (a, b) = f x
-    in  b :- sqcUnfold f a
--- Like unfoldr, but for Sqc
+    in  b :- streamUnfold f a
+-- Like unfoldr, but for Stream
 -- Doesn't require the Maybe type in the signature,
 -- since the list never ends
 
-sqcCycle :: forall a. [a] -> Maybe (Sqc a)
-sqcCycle [] = Nothing
-sqcCycle l = Just $ sqcFromList (cycle l) undefined
--- Like cycle, but for Sqc
+streamCycle :: forall a. [a] -> Maybe (Stream a)
+streamCycle [] = Nothing
+streamCycle l = Just $ streamFromList (cycle l) undefined
+-- Like cycle, but for Stream
 -- Safer, returns Nothing when the input is an empty list
