5fb4bf5b31a032a6e3c1446dd55521a6a725fca1
schuelermine <nitrogenfan@gmail.com>
2019-06-12 00:40:54 +0200

    Moved certain Array functions to instances of Array

--- Experimental/Array.hs
+++ Experimental/Array.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE ViewPatterns #-}
 
-module Experimental.Array (Array(), fromFunction, fromTotalFunction, getItem, set, remove, fromList, getItems, slice, zipArray, supplement, genericGetItems, empty, mono) where
+module Experimental.Array (Array(), fromFunction, fromTotalFunction, getItem, set, remove, fromList, getItems, slice, genericGetItems, empty, mono) where
 
 import Data.Natural
+import Control.Applicative
 
 newtype Array x = Array { getItem :: Natural -> Maybe x }
 
@@ -17,6 +18,9 @@ fromFunction = Array
 fromTotalFunction :: (Natural -> x) -> Array x
 fromTotalFunction = Array . (Just .)
 
+fromList :: [x] -> Array x
+fromList xs = Array (xs !!?)
+
 set :: Natural -> x -> Array x -> Array x
 set n x a = Array $ \m ->
     case n == m of
@@ -29,44 +33,33 @@ remove n a = Array $ \m ->
         True -> Nothing
         False -> getItem a m
 
-instance Functor Array where
-    fmap f a = Array $ \n ->
-        f <$> (getItem a n)
-
-arrayApply :: Array (x -> y) -> Array x -> Array y
-arrayApply f a = Array $ \n ->
-    ($) <$> getItem f n <*> getItem a n
-
-instance Applicative Array where
-    pure = mono
-    (<*>) = arrayApply
-
-fromList :: [x] -> Array x
-fromList xs = Array (xs !!?)
-
 getItems :: [Natural] -> Array x -> [Maybe x]
 getItems ns array = (getItem array) <$> ns
 
 slice :: Natural -> Natural -> Array x -> [Maybe x]
 slice n m = getItems [n..m]
 
-zipArray :: Array x -> Array y -> Array (x, y)
-zipArray a b = (,) <$> a <*> b
-
-supplement :: Array x -> Array x -> Array x
-supplement a b = Array $ \n ->
-    case getItem a n of
-        Nothing -> getItem b n
-        x -> x
-
 genericGetItems :: Functor f => f Natural -> Array x -> f (Maybe x)
 genericGetItems ns a = (getItem a) <$> ns
-
-empty :: Array x
-empty = Array $ const Nothing
 
 mono :: x -> Array x
 mono x = Array . const $ Just x
 
 instance Show x => Show (Array x) where
     show a = show $ getItems [0..] a
+
+instance Functor Array where
+    fmap f a = Array $ \n ->
+        f <$> (getItem a n)
+
+instance Applicative Array where
+    pure x = Array . const $ Just x
+    f <*> a = Array $ \n ->
+        ($) <$> getItem f n <*> getItem a n
+
+instance Alternative Array where
+    empty = Array $ const Nothing
+    a <|> b = Array $ \n ->
+        case getItem a n of
+            Nothing -> getItem b n
+            x -> x
