0cd175dc5765dcbf6c8f34a557c7a274be8ecf42
schuelermine <nitrogenfan@gmail.com>
2019-06-12 15:17:50 +0200

    Wrote SVector and ListArray

---
+++ Experimental/ListArray.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DataKinds, ConstraintKinds, GADTs, TypeFamilies, MultiParamTypeClasses, ExplicitForAll, FlexibleInstances, UndecidableInstances, QuantifiedConstraints, RankNTypes, TypeOperators, FunctionalDependencies, ScopedTypeVariables, KindSignatures, NoStarIsType, PolyKinds #-}
+
+module Experimental.ListArray where
+
+import Data.Finite
+import Data.Singletons.TypeLits
+
+data VectorArray n x where
+    VectorArray :: SNat n -> (Finite n -> x) -> VectorArray n x
+
+getItem :: VectorArray n x -> Finite n -> x
+getItem (VectorArray _ f) = f
+getLength :: VectorArray n x -> SNat n
+getLength (VectorArray sn _) = sn
---
+++ Experimental/SVector.hs
@@ -0,0 +1,6 @@
+module Experimental.SVector where
+
+import Data.Singletons.TypeLits
+
+data SVector n m where
+    SVector :: 
