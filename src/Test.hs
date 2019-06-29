b5a39bf02dd3ca9274d719dbdba246208e5eda98
schuelermine <nitrogenfan@gmail.com>
2019-06-29 14:27:45 +0200

    Effectively removed useless modules

--- src/Test.hs
+++
@@ -1,6 +0,0 @@
--- Intended to test if you can export things you don't import. No you can't.
--- ! Non-functional
-
-module Test(Cirq(Cirq)) where
-
-import Cirq.Basic
--- src/Useless.hs
+++
@@ -1,2 +0,0 @@
-module Useless where
--- This module is useless
