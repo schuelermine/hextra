c9ac10fc514b89add0367920d292365a6f6d490c
schuelermine <git@anselmschueler.com>
2020-06-29 19:58:43 +0200

    something

--- src/Extra/List.hs
+++ src/Extra/List.hs
@@ -1,6 +1,18 @@
-{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE ExplicitForAll, TypeApplications #-}
 
 module Extra.List where
+
+genericReplace :: forall n a. Integral n => n -> a -> [a] -> [a]
+genericReplace _ _ [] = []
+genericReplace 0 a (_:xs) = a : xs
+genericReplace n a l@(x:xs)
+    | n <= 0 = l
+    | True = x : genericReplace (n - 1) a xs
+
+replace = genericReplace @Int
+-- TODO Debate if this should be Integer instead.
+
+
 
 replaceTail :: forall a. [a] -> [a] -> [a]
 replaceTail [] _ = []
