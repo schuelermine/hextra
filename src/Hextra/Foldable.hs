319e7ea0839f8644b96f0245fbbd9d25872a9b55
schuelermine <git@anselmschueler.com>
2020-12-02 00:26:06 +0100

    swap f for t

--- src/Hextra/Foldable.hs
+++ src/Hextra/Foldable.hs
@@ -2,14 +2,14 @@
 
 module Hextra.Foldable where
 
-safeFoldr1 :: forall f a. Foldable f => (a -> a -> a) -> f a -> Maybe a
+safeFoldr1 :: forall t a. Foldable t => (a -> a -> a) -> t a -> Maybe a
 safeFoldr1 f = foldr q Nothing where
     q x Nothing = Just x
     q x (Just y) = Just $ f x y
 
-safeMaximum :: forall f a. (Foldable f, Ord a) => f a -> Maybe a
+safeMaximum :: forall t a. (Foldable t, Ord a) => t a -> Maybe a
 safeMaximum = safeFoldr1 max
-safeMinimum :: forall f a. (Foldable f, Ord a) => f a -> Maybe a
+safeMinimum :: forall t a. (Foldable t, Ord a) => t a -> Maybe a
 safeMinimum = safeFoldr1 min
 
 weigh :: forall t n a. (Foldable t, Num n) => t (n, a -> Bool) -> a -> n
