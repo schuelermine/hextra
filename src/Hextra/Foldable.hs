281261c1369dd40444af703c5c7fb4616e852009
schuelermine <git@anselmschueler.com>
2020-12-03 12:05:56 +0100

    add foldrUntil

--- src/Hextra/Foldable.hs
+++ src/Hextra/Foldable.hs
@@ -15,3 +15,18 @@ safeMinimum = safeFoldr1 min
 weigh :: forall t n a. (Foldable t, Num n) => t (n, a -> Bool) -> a -> n
 weigh ws x = foldr ((+) . f) 0 ws where
     f (n, p) = if p x then n else 0
+
+foldrUntil1 :: Foldable t => (a -> b -> b) -> (b -> Bool) -> b -> t a -> (Bool, b)
+foldrUntil1 f p i = foldr g (True, i) where
+    g _ (False, y) = (False, y)
+    g x (True, y) = if p y then (False, y) else (True, f x y)
+
+foldrUntil2 :: Foldable t => (a -> b -> b) -> (a -> Bool) -> b -> t a -> (Bool, b)
+foldrUntil2 f p i = foldr g (True, i) where
+    g _ (False, y) = (False, y)
+    g x (True, y) = if p x then (False, y) else (True, f x y)
+
+foldrUntil3 :: Foldable t => (a -> b -> b) -> (b -> Bool) -> b -> t a -> (Bool, b)
+foldrUntil3 f p i = foldr g (True, i) where
+    g _ (False, y) = (False, y)
+    g x (True, y) = if p (f x y) then (False, y) else (True, f x y)
